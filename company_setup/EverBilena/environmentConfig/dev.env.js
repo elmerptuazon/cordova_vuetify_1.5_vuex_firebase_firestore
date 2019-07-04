ba37faa00015c382a48e73421a0ec878ce5c8c30
Albert Laure <albertlaure@Alberts-MacBook-Pro.local>
2019-07-04 12:27:38 +0800

    added environmentsetup for EB

--- company_setup/EverBilena/environmentConfig/dev.env.js
+++ company_setup/EverBilena/environmentConfig/dev.env.js
@@ -2,5 +2,12 @@ var merge = require('webpack-merge')
 var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"'
+  NODE_ENV: '"development"',
+  apiKey: '"AIzaSyCSx6lIrUmXflCVFe6DKktZwXuXvbfZtJk"',
+  authDomain: '"ever-bilena-dev.firebaseapp.com"',
+  databaseURL: '"https://ever-bilena-dev.firebaseio.com"',
+  projectId: '"ever-bilena-dev"',
+  storageBucket: '"ever-bilena-dev.appspot.com"',
+  messagingSenderId: '"127418362624"',
+  appId: '"1:127418362624:web:fe8dafbb3c63c6d9"'
 })
--- company_setup/EverBilena/environmentConfig/prod.env.js
+++ company_setup/EverBilena/environmentConfig/prod.env.js
@@ -1,3 +1,10 @@
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+  apiKey: '"AIzaSyCeGcbnkddmwFtTKmNafcoXpUU1nE7Cr1I"',
+  authDomain: '"ever-bilena.firebaseapp.com"',
+  databaseURL: '"https://ever-bilena.firebaseio.com"',
+  projectId: '"ever-bilena"',
+  storageBucket: '"ever-bilena.appspot.com"',
+  messagingSenderId: '"291939986733"',
+  appId: '"1:291939986733:web:82beda3f7ba891db"'
 }
