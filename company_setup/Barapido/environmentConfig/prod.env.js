688136ccbb39b8821f029ef1ddfac5bf9acd324a
Albert Laure <albertlaure@Alberts-MacBook-Pro.local>
2019-07-08 18:48:24 +0800

    added environment Config for barapido

---
+++ company_setup/Barapido/environmentConfig/prod.env.js
@@ -0,0 +1,14 @@
+module.exports = {
+  NODE_ENV: '"production"',
+  apiKey: '"AIzaSyCeGcbnkddmwFtTKmNafcoXpUU1nE7Cr1I"',
+  authDomain: '"ever-bilena.firebaseapp.com"',
+  databaseURL: '"https://ever-bilena.firebaseio.com"',
+  projectId: '"ever-bilena"',
+  storageBucket: '"ever-bilena.appspot.com"',
+  messagingSenderId: '"291939986733"',
+  appId: '"1:291939986733:web:82beda3f7ba891db"',
+  storageCode: '"ever-bilena"',
+  webAdminId: '"admin"',
+  companyName: '"Ever Bilena"',
+  version: '"0.6.5"'
+}
