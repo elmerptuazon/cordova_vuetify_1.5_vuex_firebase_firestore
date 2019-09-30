296cd81b6ea2804931d0c2be22b45eb61ace0755
Albert Laure <albertlaure@Alberts-MacBook-Pro.local>
2019-09-30 16:45:56 +0800

    updated Checkout page, added cc form

---
+++ company_setup/Barapido/src/components/creditCardForm.vue
@@ -0,0 +1,64 @@
+<template>
+  <v-container>
+    <v-layout align-center justify-center row fill-height>
+      <v-flex xs12>
+        <p class="body-2">Credit Card Number:</p>
+        <v-text-field
+          single-line
+          outline
+          v-cardformat:formatCardNumber
+          v-model="cardDetails.cardNumber"
+          prepend-icon="credit_card"
+        >
+        </v-text-field>
+      </v-flex>
+    </v-layout>
+    <v-layout align-center justify-center row fill-height>
+      <v-flex xs7>
+        <p class="body-2">Expiry:</p>
+        <v-text-field
+          single-line
+          outline
+          v-cardformat:formatCardExpiry
+          v-model="cardDetails.expiry"
+        >
+        </v-text-field>
+      </v-flex>
+      <v-flex xs5 ml-1>
+        <p class="body-2">CVC:</p>
+        <v-text-field
+          single-line
+          outline
+          v-cardformat:formatCardCVC
+          v-model="cardDetails.CVC"
+        >
+        </v-text-field>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    cardDetails: {
+      cardNumber: null,
+      expiry: null,
+      CVC: null
+    }
+  }),
+  mounted() {},
+  methods: {},
+  watch: {
+    cardDetails: {
+      handler(values) {
+        this.$emit("cardDetails", values);
+      },
+      deep: true
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
