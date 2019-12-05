58cc8e3b8c78410c021563a2b5000b4adafb50db
Albert Laure <albertlaure@Alberts-MacBook-Pro.local>
2019-12-05 14:16:16 +0800

    created providers js

---
+++ company_setup/Barapido/src/store/modules/providers.js
@@ -0,0 +1,98 @@
+import { DB, COLLECTION, FIRESTORE } from '@/config/firebaseInit';
+
+const providers = {
+    namespaced: true,
+    state: {
+        paymentProvider: {},
+        logisticsProvider: [],
+        paymentProviderSubscriber: null,
+        logisticsProviderSubscriber: null
+    },
+    getters: {
+
+    },
+    mutations: {
+
+        SetPaymentProvider(state, payload) {
+            state.paymentProvider = payload;
+        },
+        SetlogisticsProvider(state, payload) {
+            state.logisticsProvider = payload;
+        },
+        AddLogisticProvider(state, payload) {
+            state.logisticsProvider.push(payload);
+        },
+        RemoveLogisticProvider(state, payload) {
+            const filteredLogistics = state.logisticsProvider.filter(logistic => logistic.id != payload.id)
+            state.logisticsProvider = filteredLogistics;
+        },
+        UnsubscribeToPaymentSubscriber(state) {
+            state.paymentProviderSubscriber();
+        },
+        UnsubscribeToLogisticsSubscriber(state) {
+            state.logisticsProviderSubscriber();
+        }
+    },
+    actions: {
+
+
+        ListenToPaymentProvider({ state, commit }) {
+
+            state.paymentProviderSubscriber = db.collection("providers").where("providerType", "==", "payment")
+                .onSnapshot((snapshot) => {
+                    snapshot.docChanges().forEach((change) => {
+                        let paymentProviderData = change.doc.data();
+                        paymentProviderData.id = change.doc.id;
+                        if (change.type === "added") {
+                            console.log("Payment Provider: ", paymentProviderData);
+                            if (paymentProviderData.isActive) {
+                                commit('SetPaymentProvider', paymentProviderData)
+                            }
+                        }
+                        if (change.type === "modified") {
+                            console.log("Modified paymentProvider: ", paymentProviderData);
+                            if (paymentProviderData.isActive) {
+                                commit('SetPaymentProvider', paymentProviderData)
+                            }
+                        }
+
+                    });
+                });
+
+
+        },
+        ListenToLogisticsProvider({ state, commit }) {
+
+            state.logisticsProviderSubscriber = db.collection("providers").where("providerType", "==", "logistics")
+                .onSnapshot((snapshot) => {
+                    snapshot.docChanges().forEach((change) => {
+                        let logisticProviderData = change.doc.data();
+                        logisticProviderData.id = change.doc.id;
+                        if (change.type === "added") {
+                            console.log("Logistics: ", logisticProviderData);
+                            if (logisticProviderData.isActive) {
+                                commit('AddLogisticProvider', logisticProviderData)
+                            }
+                        }
+                        if (change.type === "modified") {
+                            console.log("Modified logistics Provider: ", logisticProviderData);
+
+                            if (logisticProviderData.isActive) {
+                                commit('AddLogisticProvider', logisticProviderData)
+                            }
+                            if (logisticProviderData.isActive) {
+                                commit('RemoveLogisticProvider', logisticProviderData)
+                            }
+                        }
+
+                    });
+                });
+
+
+        },
+
+
+    }
+}
+
+export default providers;
