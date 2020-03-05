33bb172482d291df50e55e67596fa2e0512f7a59
Miguel Mendoza <mendozajuanmiguel11@gmail.com>
2020-03-05 16:47:57 +0800

    added Mother Nature in company setup

--- config/companySetup.js
+++ config/companySetup.js
@@ -27,6 +27,7 @@ async function copyCompanySetUp() {
             { title: 'Ever Bilena', value: 'EverBilena' },
             { title: 'Barapido', value: 'Barapido' },
             { title: 'Fetch', value: 'Fetch' },
+            { title: 'Mother Nature', value: 'Mother Nature' },
             { title: 'AppSellTestApp', value: 'AppSellTestApp' },
             { title: 'Aquitek', value: 'Aquitek' },
             { title: 'Mary Kay', value: 'MaryKay', disabled: true },
