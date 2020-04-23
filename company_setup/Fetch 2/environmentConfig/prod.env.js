32ad91c4e5b158414620a617d81cb97c2304ca56
Juan Miguel Mendoza <mendozajuanmiguel11@gmail.com>
2020-04-23 13:02:20 +0800

    created duplicated Fetch directory

---
+++ company_setup/Fetch 2/environmentConfig/prod.env.js
@@ -0,0 +1,6 @@
+module.exports = {
+  NODE_ENV: '"production"',
+  lalamoveURL: '"https://production-rest.lalamove.com"',
+  companyAddress: "'170, Salcedo St. Legazpi Village, Makati City, Metro Manila, 1229'",
+  contactNo: '"09123456789"'
+}
