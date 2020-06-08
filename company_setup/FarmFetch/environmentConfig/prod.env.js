257e1ffb73bc8a1ca91c37a4a916a3ceb1fb4af6
Miggy Mendoza <mendozajuanmiguel11@gmail.com>
2020-06-08 15:34:13 +0800

    adjusted environmental configs for Farm Fetch

---
+++ company_setup/FarmFetch/environmentConfig/prod.env.js
@@ -0,0 +1,18 @@
+module.exports = {
+  NODE_ENV: '"production"',
+  apiKey: '"AIzaSyDYoaR6mB5exEIkO2_1BJKoMuhUiSo2mnY"',
+  authDomain: '"farmfetchdev-7031c.firebaseapp.com"',
+  databaseURL: '"https://farmfetchdev-7031c.firebaseio.com"',
+  projectId: '"farmfetchdev-7031c"',
+  storageBucket: '"farmfetchdev-7031c.appspot.com"',
+  messagingSenderId: '"455899115382"',
+  appId: '"1:455899115382:web:512d68156916c929491457"',
+  storageCode: '"FarmFetchDev"',
+  webAdminId: '"admin"',
+  companyName: '"Farm Fetch"',
+  contactNumber: '"09123456789"',
+  version: '"0.6.10"',
+  primaryColor: '"#90C83E"',
+  environment: '"live"',
+  callbackURL: '"https://us-central1-appselltestapp.cloudfunctions.net/callback/checkPaymentStatus"'
+}
---
+++ company_setup/FarmFetch/environmentConfig/test.env.js
@@ -0,0 +1,21 @@
+var merge = require('webpack-merge')
+var devEnv = require('./dev.env')
+
+module.exports = merge(devEnv, {
+  NODE_ENV: '"testing"',
+  apiKey: '"AIzaSyDYoaR6mB5exEIkO2_1BJKoMuhUiSo2mnY"',
+  authDomain: '"farmfetchdev-7031c.firebaseapp.com"',
+  databaseURL: '"https://farmfetchdev-7031c.firebaseio.com"',
+  projectId: '"farmfetchdev-7031c"',
+  storageBucket: '"farmfetchdev-7031c.appspot.com"',
+  messagingSenderId: '"455899115382"',
+  appId: '"1:455899115382:web:512d68156916c929491457"',
+  storageCode: '"FarmFetchDev"',
+  webAdminId: '"admin"',
+  companyName: '"Farm Fetch"',
+  contactNumber: '"09123456789"',
+  version: '"0.6.10"',
+  primaryColor: '"#90C83E"',
+  environment: '"test"',
+  callbackURL: '"https://us-central1-appselltestapp.cloudfunctions.net/callback/checkPaymentStatus"'
+})
