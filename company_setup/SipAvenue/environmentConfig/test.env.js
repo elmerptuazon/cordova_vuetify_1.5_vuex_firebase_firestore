a3ef0ec0aa147d2f272c936bb8a439ea71ab7f3c
China Marie Lao <china.appsell@gmail.com>
2020-06-10 11:03:19 +0800

    created directory for Sip Avenue

---
+++ company_setup/SipAvenue/environmentConfig/test.env.js
@@ -0,0 +1,21 @@
+var merge = require('webpack-merge')
+var devEnv = require('./dev.env')
+
+module.exports = merge(devEnv, {
+  NODE_ENV: '"testing"',
+  apiKey: '"AIzaSyA1p43Hys8LeHoqINSFOItJDEHLmggy4C4"',
+  authDomain: '"potatocornerdev.firebaseapp.com"',
+  databaseURL: '"https://potatocornerdev.firebaseio.com"',
+  projectId: '"potatocornerdev"',
+  storageBucket: '"potatocornerdev.appspot.com"',
+  messagingSenderId: '"846971526982"',
+  appId: '"1:846971526982:web:027bf465e5be6cc53e8f47"',
+  storageCode: '"PotatoCorner"',
+  webAdminId: '"admin"',
+  companyName: '"Potato Corner"',
+  contactNumber: '"09123456789"',
+  version: '"0.6.10"',
+  primaryColor: '"#119247"',
+  environment: '"test"',
+  callbackURL: '"https://us-central1-appselltestapp.cloudfunctions.net/callback/checkPaymentStatus"'
+})
