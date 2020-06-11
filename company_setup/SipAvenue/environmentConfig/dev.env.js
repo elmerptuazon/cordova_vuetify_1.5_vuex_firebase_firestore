1aff33b698e7372c0db6b1d7e2e598f1515fd441
Miggy Mendoza <mendozajuanmiguel11@gmail.com>
2020-06-11 15:02:40 +0800

    added frebase config for Sip Avenue

--- company_setup/SipAvenue/environmentConfig/dev.env.js
+++ company_setup/SipAvenue/environmentConfig/dev.env.js
@@ -3,19 +3,19 @@ var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  apiKey: '"AIzaSyA1p43Hys8LeHoqINSFOItJDEHLmggy4C4"',
-  authDomain: '"potatocornerdev.firebaseapp.com"',
-  databaseURL: '"https://potatocornerdev.firebaseio.com"',
-  projectId: '"potatocornerdev"',
-  storageBucket: '"potatocornerdev.appspot.com"',
-  messagingSenderId: '"846971526982"',
-  appId: '"1:846971526982:web:027bf465e5be6cc53e8f47"',
-  storageCode: '"PotatoCorner"',
+  projectId: '"sipavenue-dev"',
+  appId: '"1:509789194682:web:150e0de65622e8d347f583"',
+  databaseURL: '"https://sipavenue-dev.firebaseio.com"',
+  storageBucket: '"sipavenue-dev.appspot.com"',
+  apiKey: '"AIzaSyAg7uAQGGRQ5VrzAYrozbjEyur-QJSpBsE"',
+  authDomain: '"sipavenue-dev.firebaseapp.com"',
+  messagingSenderId: '"509789194682"',
+  storageCode: '"SipAvenue"',
   webAdminId: '"admin"',
   companyName: '"Sip Avenue"',
   contactNumber: '"09123456789"',
   version: '"0.6.10"',
   primaryColor: '"#f7a978"',
   environment: '"test"',
-  callbackURL: '"https://us-central1-appselltestapp.cloudfunctions.net/callback/checkPaymentStatus"'
+  callbackURL: '"https://us-central1-sipavenue-dev.cloudfunctions.net/callback/checkPaymentStatus"'
 })
