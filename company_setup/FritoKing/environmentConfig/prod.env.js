3e97df399f21be9fd09ca57b6eeb769eb34ee953
Miggy Mendoza <mendozajuanmiguel11@gmail.com>
2020-06-15 10:30:01 +0800

    Added directory for FritoKing

---
+++ company_setup/FritoKing/environmentConfig/prod.env.js
@@ -0,0 +1,18 @@
+module.exports = {
+  NODE_ENV: '"production"',
+  apiKey: '"AIzaSyA1p43Hys8LeHoqINSFOItJDEHLmggy4C4"',
+  authDomain: '"potatocornerdev.firebaseapp.com"',
+  databaseURL: '"https://potatocornerdev.firebaseio.com"',
+  projectId: '"potatocornerdev"',
+  storageBucket: '"potatocornerdev.appspot.com"',
+  messagingSenderId: '"846971526982"',
+  appId: '"1:846971526982:web:027bf465e5be6cc53e8f47"',
+  storageCode: '"PotatoCorner"',
+  webAdminId: '"admin"',
+  companyName: '"Potato Corner"',
+  contactNumber: '"09123456789"',
+  version: '"0.6.10"',
+  primaryColor: '"#119247"',
+  environment: '"live"',
+  callbackURL: '"https://us-central1-appselltestapp.cloudfunctions.net/callback/checkPaymentStatus"'
+}
