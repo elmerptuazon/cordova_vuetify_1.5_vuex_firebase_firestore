43fbf83d50ab28d5ef163d7d6a79a2c98c77a562
China Marie Lao <china.appsell@gmail.com>
2020-06-29 19:33:40 +0800

    created directory for AppSell Franchise

---
+++ company_setup/AppSellFranchise/environmentConfig/dev.env.js
@@ -0,0 +1,21 @@
+var merge = require('webpack-merge')
+var prodEnv = require('./prod.env')
+
+module.exports = merge(prodEnv, {
+  NODE_ENV: '"development"',
+  projectId: '"fritoking-dev"',
+  appId: '"1:200493591511:web:086dcbfe7f53d720aef992"',
+  databaseURL: '"https://fritoking-dev.firebaseio.com"',
+  storageBucket: '"fritoking-dev.appspot.com"',
+  apiKey: '"AIzaSyAKjz6J06tTgQHM4kqnTod45eOkXR9_NOQ"',
+  authDomain: '"fritoking-dev.firebaseapp.com"',
+  messagingSenderId: '"200493591511"',
+  storageCode: '"FritoKing"',
+  webAdminId: '"admin"',
+  companyName: '"Frito King"',
+  contactNumber: '"09123456789"',
+  version: '"0.6.10"',
+  primaryColor: '"#c11515"',
+  environment: '"test"',
+  callbackURL: '"https://us-central1-fritoking-dev.cloudfunctions.net/callback/checkPaymentStatus"',
+})
