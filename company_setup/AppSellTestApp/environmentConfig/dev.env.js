9f6f1e9ec6c4ecbf210059b6521ddd39df39be64
elmer <elmerptuazon@gmail.com>
2020-07-09 14:13:28 +0800

    version 6.15 pushed

--- company_setup/AppSellTestApp/environmentConfig/dev.env.js
+++ company_setup/AppSellTestApp/environmentConfig/dev.env.js
@@ -15,7 +15,7 @@ module.exports = merge(prodEnv, {
   webAdminId: '"admin"',
   companyName: '"AppSellTestApp"',
   contactNumber: '"09123456789"',
-  version: '"0.6.13"',
+  version: '"0.6.15"',
   primaryColor: '"#0E1E38"',
   environment: '"test"',
   callbackURL: '"https://us-central1-appselltestapp.cloudfunctions.net/callback"'
