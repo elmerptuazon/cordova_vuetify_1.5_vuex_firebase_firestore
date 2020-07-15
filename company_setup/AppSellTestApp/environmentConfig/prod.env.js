35a7ad237aedbb4b7e65c94c4811ddc304d0a3ee
elmer <elmerptuazon@gmail.com>
2020-07-15 10:35:38 +0800

    update version

--- company_setup/AppSellTestApp/environmentConfig/prod.env.js
+++ company_setup/AppSellTestApp/environmentConfig/prod.env.js
@@ -12,7 +12,7 @@ module.exports = {
   webAdminId: '"admin"',
   companyName: '"AppSellTestApp"',
   contactNumber: '"09123456789"',
-  version: '"0.6.15"',
+  version: '"0.6.16"',
   primaryColor: '"#0E1E38"',
   environment: '"live"',
   callbackURL: '"https://us-central1-appselltestapp.cloudfunctions.net/callback"'
--- company_setup/AppSellTestApp/environmentConfig/test.env.js
+++ company_setup/AppSellTestApp/environmentConfig/test.env.js
@@ -15,7 +15,7 @@ module.exports = merge(devEnv, {
   webAdminId: '"admin"',
   companyName: '"AppSellTestApp"',
   contactNumber: '"09123456789"',
-  version: '"0.6.15"',
+  version: '"0.6.16"',
   primaryColor: '"#0E1E38"',
   environment: '"test"',
   callbackURL: '"https://us-central1-appselltestapp.cloudfunctions.net/callback"'
