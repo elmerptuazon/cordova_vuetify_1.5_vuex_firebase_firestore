7a979d4ae35b7ae727798802eff805f60868bccb
Miggy Mendoza <miggymendoza@Miggys-Mac.local>
2020-08-09 20:32:46 +0800

    added script for generating ios app

--- config/dirfix.js
+++ config/dirfix.js
@@ -1,6 +1,8 @@
 const fs = require('fs-extra');
 const Confirm = require('prompt-confirm');
-const { exec } = require('child_process');
+const {
+  exec
+} = require('child_process');
 const companySetup = require('./companySetup');
 
 const srcpath = 'www/fonts/';
@@ -9,43 +11,43 @@ const indexFile = 'www/index.html';
 
 
 function executeCommand(cmd) {
-	return new Promise(function (resolve, reject) {
-		exec(cmd, function (err, stdout) {
-			if (err) {
-				reject(err);
-				return;
-			}
+  return new Promise(function (resolve, reject) {
+    exec(cmd, function (err, stdout) {
+      if (err) {
+        reject(err);
+        return;
+      }
 
-			resolve(stdout);
-		});
-	});
+      resolve(stdout);
+    });
+  });
 }
 
 (async function fixDirectoriesAndFilesOfWWW() {
-	try {
-		console.log('Setting up the company')
-		const value = await companySetup.copyCompanySetUp();
-		//console.log(value);
-		console.log('Building app. Please wait...');
-		await executeCommand('npm run build');
-		console.log('Build done.');
-		const data = await fs.readFile(indexFile, 'utf8');
-		const index = data.replace(/\/static/g, '');
-		await fs.outputFile(indexFile, index);
-		await fs.move(srcpath, dstpath);
-		console.log('Directories and files of www fixed.');
+  try {
+    console.log('Setting up the company')
+    const value = await companySetup.copyCompanySetUp();
+    //console.log(value);
+    console.log('Building app. Please wait...');
+    await executeCommand('npm run build');
+    console.log('Build done.');
+    const data = await fs.readFile(indexFile, 'utf8');
+    const index = data.replace(/\/static/g, '');
+    await fs.outputFile(indexFile, index);
+    await fs.move(srcpath, dstpath);
+    console.log('Directories and files of www fixed.');
 
-		// const prompt = new Confirm('Generate apk file?');
-		// const answer = await prompt.run();
-		const answer = true;
-		if (answer) {
-			console.log('Building apk. Please wait...');
-			await executeCommand('cordova build ios');
-			console.log('APK generated.');
-			//await executeCommand(String.raw`start platforms\android\app\build\outputs\apk\debug`);
-		}
-	} catch (err) {
-		console.error(err);
-		throw err;
-	}
+    // const prompt = new Confirm('Generate apk file?');
+    // const answer = await prompt.run();
+    // const answer = true;
+    // if (answer) {
+    //   console.log('Building apk. Please wait...');
+    //   await executeCommand('cordova build android');
+    //   console.log('APK generated.');
+    //   //await executeCommand(String.raw`start platforms\android\app\build\outputs\apk\debug`);
+    // }
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
 })();
---
+++ config/dirfix_ios.js
@@ -0,0 +1,51 @@
+const fs = require('fs-extra');
+const Confirm = require('prompt-confirm');
+const { exec } = require('child_process');
+const companySetup = require('./companySetup');
+
+const srcpath = 'www/fonts/';
+const dstpath = 'www/css/fonts';
+const indexFile = 'www/index.html';
+
+
+function executeCommand(cmd) {
+	return new Promise(function (resolve, reject) {
+		exec(cmd, {maxBuffer: 1024 * 500}, function (err, stdout) {
+			if (err) {
+				reject(err);
+				return;
+      }
+
+      resolve(stdout);
+    });
+	});
+}
+
+(async function fixDirectoriesAndFilesOfWWW() {
+	try {
+		console.log('Setting up the company')
+		const value = await companySetup.copyCompanySetUp();
+		//console.log(value);
+		console.log('Building app. Please wait...');
+		await executeCommand('npm run build');
+		console.log('Build done.');
+		const data = await fs.readFile(indexFile, 'utf8');
+		const index = data.replace(/\/static/g, '');
+		await fs.outputFile(indexFile, index);
+		await fs.move(srcpath, dstpath);
+		console.log('Directories and files of www fixed.');
+
+		// const prompt = new Confirm('Generate apk file?');
+		// const answer = await prompt.run();
+		// const answer = true;
+		// if (answer) {
+		// 	console.log('Building iOS. Please wait...');
+		// 	await executeCommand('cordova build ios');
+		// 	console.log('iOS generated.');
+		// 	//await executeCommand(String.raw`start platforms\android\app\build\outputs\apk\debug`);
+		// }
+	} catch (err) {
+		console.error(err);
+		throw err;
+	}
+})();
