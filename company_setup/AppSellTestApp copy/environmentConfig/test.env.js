e52e42b07f42a9e99699e4181af780bb9e15552c
China Marie Lao <china.appsell@gmail.com>
2020-08-24 18:16:43 +0800

    copied custom files of Fetch

--- company_setup/AppSellTestApp copy/environmentConfig/test.env.js
+++ company_setup/AppSellTestApp copy/environmentConfig/test.env.js
@@ -3,20 +3,18 @@ var devEnv = require('./dev.env')
 
 module.exports = merge(devEnv, {
   NODE_ENV: '"testing"',
-  apiKey: '"AIzaSyDAxQ8dvBH2CQNkvoFuklqVRV76KaRyKJ0"',
-  authDomain: '"appselltestapp.firebaseapp.com"',
-  databaseURL: '"https://appselltestapp.firebaseio.com"',
-  projectId: '"appselltestapp"',
-  storageBucket: '"appselltestapp.appspot.com"',
-  messagingSenderId: '"671506060709"',
-  appId: '"1:671506060709:web:7bcc28481ba6b94d12044c"',
-  measurementId: '"G-XV2JER7LNT"',
-  storageCode: '"AppSellTestApp"',
+  apiKey: '"AIzaSyDboEkHtRlIxudB7NIHyC-H3aFKswLfReA"',
+  authDomain: '"fetch-dev-a1f39.firebaseapp.com"',
+  databaseURL: '"https://fetch-dev-a1f39.firebaseio.com"',
+  projectId: '"fetch-dev-a1f39"',
+  storageBucket: '"fetch-dev-a1f39.appspot.com"',
+  messagingSenderId: '"769658614979"',
+  appId: '"1:769658614979:web:0c1b3f1f986ddc1c0cde76"',
+  primaryColor: '"#C6863F"',
+  storageCode: '"fetch"',
   webAdminId: '"admin"',
-  companyName: '"AppSellTestApp"',
-  contactNumber: '"09123456789"',
-  version: '"0.6.10"',
-  primaryColor: '"#0E1E38"',
+  companyName: '"Fetch"',
+  version: '"0.6.7"',
   environment: '"test"',
-  callbackURL: '"https://us-central1-appselltestapp.cloudfunctions.net/callback"'
+  callbackURL: '"https://us-central1-fetch-dev-a1f39.cloudfunctions.net/callback/checkPaymentStatus"'
 })
