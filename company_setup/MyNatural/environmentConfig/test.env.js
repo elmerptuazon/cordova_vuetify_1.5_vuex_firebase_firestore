a637cbb727efb182bb143b3668c2d5c5229d8d3f
China Marie Lao <china.appsell@gmail.com>
2020-09-07 18:39:35 +0800

    adjusted env config of My Natural

--- company_setup/MyNatural/environmentConfig/test.env.js
+++ company_setup/MyNatural/environmentConfig/test.env.js
@@ -3,20 +3,20 @@ var devEnv = require('./dev.env')
 
 module.exports = merge(devEnv, {
   NODE_ENV: '"testing"',
-  apiKey: '"AIzaSyDAxQ8dvBH2CQNkvoFuklqVRV76KaRyKJ0"',
-  authDomain: '"appselltestapp.firebaseapp.com"',
-  databaseURL: '"https://appselltestapp.firebaseio.com"',
-  projectId: '"appselltestapp"',
-  storageBucket: '"appselltestapp.appspot.com"',
-  messagingSenderId: '"671506060709"',
-  appId: '"1:671506060709:web:7bcc28481ba6b94d12044c"',
+  apiKey: '"AIzaSyAxxc_vzR7XpBMD3o8HP-d68o92ZrbFi7Q"',
+  authDomain: '"mynatural-dev.firebaseapp.com"',
+  databaseURL: '"https://mynatural-dev.firebaseio.com"',
+  projectId: '"mynatural-dev"',
+  storageBucket: '"mynatural-dev.appspot.com"',
+  messagingSenderId: '"329294986135"',
+  appId: '"1:329294986135:web:984cb506ca95f7c890a2d2"',
   measurementId: '"G-XV2JER7LNT"',
-  storageCode: '"AppSellTestApp"',
+  storageCode: '"mynatural-dev"',
   webAdminId: '"admin"',
-  companyName: '"AppSellTestApp"',
+  companyName: '"My Natural"',
   contactNumber: '"09123456789"',
   version: '"0.6.20"',
-  primaryColor: '"#0E1E38"',
+  primaryColor: '"#01903F"',
   environment: '"test"',
-  callbackURL: '"https://us-central1-appselltestapp.cloudfunctions.net/callback"'
+  callbackURL: '"https://us-central1-mynatural-dev.cloudfunctions.net/callback"'
 })
